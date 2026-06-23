23f185949383f236cfbbea72244ee94a43132db6
jonesdwill <jonesdwill@outlook.com>
2026-06-23 20:39:46 +0100

    blog post sidebar, hero image support, remove comments

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,14 +4,13 @@ import { z } from 'astro/zod';
 
 const blog = defineCollection({
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
 
 const pages = defineCollection({
